c617d718448a7045686e50f387e01315f9a0d106
kasra-habibbeygi <kasrahabibbeygi@gmail.com>
2023-05-08 14:51:10 +0430

    create eexam pages

--- src/components/pages/exam/questions/content.jsx
+++ src/components/pages/exam/questions/content.jsx
@@ -1,10 +1,23 @@
+/* eslint-disable no-unused-vars */
 // Assets
 import { TitleField, QuestionsField } from './content.style';
 
+// MUI
+import { FormControlLabel, Radio, RadioGroup } from '@mui/material';
+
+// Hooks
+import useTimer from '@/hooks/use-timer';
+
+// Component
+import Button from '@/components/form-group/button';
+
 const QuestionsContent = () => {
+    const [days, hours, minutes, seconds, countDown] = useTimer(300000);
+
+    const progressPercent = ((countDown - 300000) / 300000) * 100;
     return (
         <>
-            <TitleField>
+            <TitleField progressPercent={Math.abs(parseInt(progressPercent))}>
                 <div className='title'>
                     <h3>کوییز ویدیو لورم ایپسوم</h3>
                     <p>12 سوال - 25 دقیقه</p>
@@ -13,48 +26,131 @@ const QuestionsContent = () => {
                     <div className='progress'>
                         <span></span>
                     </div>
-                    <p>23 : 10</p>
+                    <p>
+                        {seconds} : {minutes}
+                    </p>
                 </div>
             </TitleField>
             <QuestionsField>
-                <p>asdasd</p>
-                <p>asdasd</p>
-                <p>asdasd</p>
-                <p>asdasd</p>
-                <p>asdasd</p>
-                <p>asdasd</p>
-                <p>asdasd</p>
-                <p>asdasd</p>
-                <p>asdasd</p>
-                <p>asdasd</p>
-                <p>asdasd</p>
-                <p>asdasd</p>
-                <p>asdasd</p>
-                <p>asdasd</p>
-                <p>asdasd</p>
-                <p>asdasd</p>
-                <p>asdasd</p>
-                <p>asdasd</p>
-                <p>asdasd</p>
-                <p>asdasd</p>
-                <p>asdasd</p>
-                <p>asdasd</p>
-                <p>asdasd</p>
-                <p>asdasd</p>
-                <p>asdasd</p>
-                <p>asdasd</p>
-                <p>asdasd</p>
-                <p>asdasd</p>
-                <p>asdasd</p>
-                <p>asdasd</p>
-                <p>asdasd</p>
-                <p>asdasd</p>
-                <p>asdasd</p>
-                <p>asdasd</p>
-                <p>asdasd</p>
-                <p>asdasd</p>
-                <p>asdasd</p>
-                <p>asdasd</p>
+                <div className='question_card'>
+                    <small>سوال 1</small>
+                    <h4>
+                        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه
+                        روزنامه و مجله در ستون و سطرآنچنان که لازم است
+                    </h4>
+                    <RadioGroup name='radio-buttons-group' className='four_choice'>
+                        <FormControlLabel
+                            value='1'
+                            control={<Radio />}
+                            label='لورم اییپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده'
+                        />
+                        <FormControlLabel
+                            value='2'
+                            control={<Radio />}
+                            label='لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده'
+                        />
+                        <FormControlLabel
+                            value='3'
+                            control={<Radio />}
+                            label='لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده  طراحان گرافیک است'
+                        />
+                        <FormControlLabel
+                            value='4'
+                            control={<Radio />}
+                            label='لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده'
+                        />
+                    </RadioGroup>
+                </div>
+                <div className='question_card'>
+                    <small>سوال 1</small>
+                    <h4>
+                        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه
+                        روزنامه و مجله در ستون و سطرآنچنان که لازم است
+                    </h4>
+                    <RadioGroup name='radio-buttons-group' className='four_choice'>
+                        <FormControlLabel
+                            value='1'
+                            control={<Radio />}
+                            label='لورم اییپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده'
+                        />
+                        <FormControlLabel
+                            value='2'
+                            control={<Radio />}
+                            label='لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده'
+                        />
+                        <FormControlLabel
+                            value='3'
+                            control={<Radio />}
+                            label='لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده  طراحان گرافیک است'
+                        />
+                        <FormControlLabel
+                            value='4'
+                            control={<Radio />}
+                            label='لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده'
+                        />
+                    </RadioGroup>
+                </div>
+                <div className='question_card'>
+                    <small>سوال 1</small>
+                    <h4>
+                        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه
+                        روزنامه و مجله در ستون و سطرآنچنان که لازم است
+                    </h4>
+                    <RadioGroup name='radio-buttons-group' className='four_choice'>
+                        <FormControlLabel
+                            value='1'
+                            control={<Radio />}
+                            label='لورم اییپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده'
+                        />
+                        <FormControlLabel
+                            value='2'
+                            control={<Radio />}
+                            label='لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده'
+                        />
+                        <FormControlLabel
+                            value='3'
+                            control={<Radio />}
+                            label='لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده  طراحان گرافیک است'
+                        />
+                        <FormControlLabel
+                            value='4'
+                            control={<Radio />}
+                            label='لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده'
+                        />
+                    </RadioGroup>
+                </div>
+                <div className='question_card'>
+                    <small>سوال 1</small>
+                    <h4>
+                        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه
+                        روزنامه و مجله در ستون و سطرآنچنان که لازم است
+                    </h4>
+                    <RadioGroup name='radio-buttons-group' className='four_choice'>
+                        <FormControlLabel
+                            value='1'
+                            control={<Radio />}
+                            label='لورم اییپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده'
+                        />
+                        <FormControlLabel
+                            value='2'
+                            control={<Radio />}
+                            label='لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده'
+                        />
+                        <FormControlLabel
+                            value='3'
+                            control={<Radio />}
+                            label='لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده  طراحان گرافیک است'
+                        />
+                        <FormControlLabel
+                            value='4'
+                            control={<Radio />}
+                            label='لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده'
+                        />
+                    </RadioGroup>
+                </div>
+                <Button color='primary' type='filled' extraClass='submit_btn'>
+                    ثبت آزمون
+                </Button>
             </QuestionsField>
         </>
     );
--- src/components/pages/exam/questions/content.style.jsx
+++ src/components/pages/exam/questions/content.style.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import styled from '@emotion/styled';
 
 export const TitleField = styled.div(props => ({
@@ -26,13 +27,31 @@ export const TitleField = styled.div(props => ({
         display: 'flex',
         alignItems: 'center',
         gap: '20px',
+        marginTop: '20px',
 
         '& .progress': {
             width: '100%',
+            borderRadius: '50px',
+            background: '#f4f4f4',
+            height: '15px',
+            position: 'relative',
 
             span: {
-                borderRadius: '100%',
-                position : ''
+                borderRadius: '50px',
+                position: 'absolute',
+                right: '0',
+                height: '100%',
+                width: `${props.progressPercent}%`,
+                background:
+                    props.progressPercent <= 40
+                        ? '#63C7AA'
+                        : props.progressPercent >= 40 && props.progressPercent <= 80
+                            ? '#E29B00'
+                            : props.progressPercent >= 80
+                                ? '#BF0000'
+                                : '',
+                display: 'block',
+                transition: 'all linear 0.2s'
             }
         },
 
@@ -42,4 +61,33 @@ export const TitleField = styled.div(props => ({
     }
 }));
 
-export const QuestionsField = styled.div({});
+export const QuestionsField = styled.div(props => ({
+    '& .four_choice': {
+        span: {
+            fontFamily: 'Vazirmatn'
+        }
+    },
+
+    '& .question_card': {
+        border: '1px solid #e2e2e2',
+        borderRadius: '12px',
+        padding: '15px',
+        marginTop: '20px',
+
+        small: {
+            color: props.theme.palette.colors.text.light,
+            fontSize: '0.9rem'
+        },
+
+        h4: {
+            color: props.theme.palette.colors.text.blackAndWhite,
+            fontSize: '1rem',
+            marginBottom: '20px'
+        }
+    },
+
+    '& .submit_btn': {
+        margin: '20px auto 0 0 ',
+        width: '180px'
+    }
+}));
