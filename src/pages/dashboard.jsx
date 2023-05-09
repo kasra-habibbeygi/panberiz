a00db5eccd857298eeeeafc4f84281e3f0ef9771
kasra-habibbeygi <kasrahabibbeygi@gmail.com>
2023-05-09 15:54:40 +0430

    add axios request hamdler

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -3,6 +3,8 @@ import Income from '@/components/pages/dashboard/income';
 import Report from '@/components/pages/dashboard/report';
 
 function Dashboard() {
+    console.log(process.env.URL);
+
     return (
         <LayoutProvider>
             <Income />
