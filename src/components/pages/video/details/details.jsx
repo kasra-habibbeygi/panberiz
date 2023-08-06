f93e7d39aca61f641c619fe42adb5eff41c7841e
kasra-habibbeygi <kasrahabibbeygi@gmail.com>
2023-08-06 17:10:31 +0330

    aadd summary description

--- src/components/pages/video/details/details.jsx
+++ src/components/pages/video/details/details.jsx
@@ -28,6 +28,7 @@ const DetailsField = ({ mediaDetails }) => {
 
     return (
         <MainField>
+            <p className='text'>{mediaDetails?.summary_description}</p>
             <p className='text'>{mediaDetails?.full_description}</p>
             <div className='footer_field'>
                 <div className='info'>
